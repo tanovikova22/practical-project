2711538af665ebd45cce935f73ad554a20fc39c8
Tanya <tanya.novikova2203@gmail.com>
2021-01-08 19:21:15 +0200

    cteated store & part of validation

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -6,28 +6,60 @@
         <v-container>
           <v-col>
             <v-col cols="12">
-              <v-text-field v-model="email" label="E-mail" required></v-text-field>
+              <v-text-field type="email" v-model="email" label="E-mail" @blur="$v.email.$touch()"></v-text-field>
+              <div
+                class="error-message"
+                v-if="$v.email.$invalid && $v.email.$dirty"
+              >Please input correct email</div>
             </v-col>
             <v-col cols="12">
-              <v-text-field v-model="password" label="Password" required></v-text-field>
+              <v-text-field
+                type="password"
+                v-model="password"
+                label="Password"
+                @blur="$v.password.$touch()"
+              ></v-text-field>
+              <div
+                class="error-message"
+                v-if="$v.email.$invalid && $v.password.$dirty"
+              >Please enter valid password</div>
             </v-col>
           </v-col>
         </v-container>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn type="submit" depressed class="primary" @submit="onSubmit">Login</v-btn>
+          <v-btn
+            type="submit"
+            depressed
+            class="primary"
+            @submit="onSubmit"
+            :disabled="disabled"
+          >Login</v-btn>
         </v-card-actions>
       </v-form>
+      <pre>{{$v.password}}</pre>
     </v-container>
   </v-flex>
 </template>
 
 <script>
+import { required, email, minLength } from "vuelidate/lib/validators";
 export default {
   data: () => ({
     email: "",
-    password: ""
+    password: "",
+    disabled: true
   }),
+  validations: {
+    password: {
+      required,
+      minLength: minLength(5)
+    },
+    email: {
+      required,
+      email
+    }
+  },
   methods: {
     onSubmit() {
       console.log(this.email + ";" + this.password);
@@ -36,4 +68,8 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+.error-message {
+  color: red;
+  font-size: 10px;
+}
 </style>
